fa58f709fddc4f6d4a87ec9a5cc69ec12b8416e9
Aaron Heiniger <aar.heiniger@gmail.com>
2023-11-30 12:06:13 +0100

    Initial setup

---
+++ template.py
@@ -0,0 +1,15 @@
+def task_one(lines):
+    pass
+
+def task_two(lines):
+    pass
+
+if __name__ == '__main__':
+    # Read input.txt
+    with open('input.txt', 'r') as f:
+        lines = f.readlines()
+    solution_one = task_one(lines)
+    solution_two = task_two(lines)
+    print(f'Task one:\n{solution_one}')
+    print()
+    print(f'Task two:\n{solution_two}')
